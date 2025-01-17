30170e04a2efd12ac1ea9c36a362b765232b660d
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-17 16:42:08 -0600

    Initial Commit

---
+++ ARDiceeTests/ARDiceeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceeTests.swift
+//  ARDiceeTests
+//
+//  Created by Isaac Urbina on 1/17/25.
+//
+
+import Testing
+@testable import ARDicee
+
+struct ARDiceeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
