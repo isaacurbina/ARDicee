16efb85588384ecaa09d09e84808b0a9a3be01d8
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-17 17:14:24 -0600

    367. Bring the Moon into Your Living Room Using AR

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,13 +22,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
 		
-		let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+		let sphere = SCNSphere(radius: 0.2)
 		let material = SCNMaterial()
 		
-		material.diffuse.contents = UIColor.red
-		cube.materials = [material]
+		material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+		sphere.materials = [material]
 		
-		let node = SCNNode(geometry: cube)
+		let node = SCNNode(geometry: sphere)
 		node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
 		
 		sceneView.scene.rootNode.addChildNode(node)
